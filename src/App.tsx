608637597afb084ae8d441d3d1a3aaebdd80ff33
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2024-05-28 15:09:43 +0200

    feat: Days in year
    List different days of the year

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { eachWeekendOfYear, getDaysInYear } from "date-fns";
+
 function App() {
+  const year = "2024";
+  const now = new Date(year);
+  const days = getDaysInYear(now);
+  const weekends = eachWeekendOfYear(now).length;
+  const weekdays = days - weekends;
+  const publicHolidays = 10;
+  const publicHolidaysOnWeekdays = 5;
+  const vacationDays = 30;
+  const sickDays = 2;
+  const workingDays =
+    weekdays - publicHolidaysOnWeekdays - vacationDays - sickDays;
+
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>{year}</h1>
+      <ul>
+        <li>Days: {days}</li>
+        <li>Weekends: {weekends}</li>
+        <li>Weekdays: {weekdays}</li>
+        <li>Public Holidays: {publicHolidays}</li>
+        <li>Vacation Days: {vacationDays}</li>
+        <li>Sick Days: {sickDays}</li>
+        <li>Working Days: {workingDays}</li>
+      </ul>
     </div>
   );
 }
